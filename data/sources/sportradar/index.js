11c86de938609ec58628bdde26de9a9917a02a53
Ray Hatfield <ray.hatfield@nextthought.com>
2018-12-29 02:47:26 -0600

    write tmp files to cache dir

--- data/sources/sportradar/index.js
+++ data/sources/sportradar/index.js
@@ -4,9 +4,9 @@ const fetchSchedule = require('./fetch-schedule');
 
 async function update () {
 	const schedule = await fetchSchedule();
-	fs.writeFile('./tmp.json', JSON.stringify(schedule, null, '	'), () => {});
+	fs.writeFile('./cache/tmp.json', JSON.stringify(schedule, null, '	'), () => {});
 	const filtered = filter(schedule);
-	fs.writeFile('./tmp-filtered.json', JSON.stringify(filtered, null, '	'), () => {});
+	fs.writeFile('./cache/tmp-filtered.json', JSON.stringify(filtered, null, '	'), () => {});
 	return filtered;
 }
 
