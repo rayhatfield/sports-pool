12d6cdb3b017ff06a4212b4cf3f773381bcb0903
Ray Hatfield <ray.hatfield@nextthought.com>
2018-12-29 18:03:10 -0600

    lint

--- data/sources/sportradar/index.js
+++ data/sources/sportradar/index.js
@@ -4,9 +4,9 @@ const fetchSchedule = require('./fetch-schedule');
 
 async function update () {
 	const schedule = await fetchSchedule();
-	fs.writeFile('./cache/tmp.json', JSON.stringify(schedule, null, '	'), () => {});
+	fs.writeFile('./cache/tmp.json', JSON.stringify(schedule, null, '	'), () => null);
 	const filtered = filter(schedule);
-	fs.writeFile('./cache/tmp-filtered.json', JSON.stringify(filtered, null, '	'), () => {});
+	fs.writeFile('./cache/tmp-filtered.json', JSON.stringify(filtered, null, '	'), () => null);
 	return filtered;
 }
 
